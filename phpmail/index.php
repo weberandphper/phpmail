78f4812d1885f4b110615f8b862b77bae086c66e
weberandphper <cfw1034846940@gmail.com>
2016-09-16 11:27:01 +0800

    phpmail   2016/9/16

---
+++ phpmail/index.php
@@ -0,0 +1,46 @@
+﻿<html>
+<head>
+<meta charset="utf-8">
+<title>email</title>
+</head>
+<body>
+<?php
+
+	set_time_limit(0); 				
+
+	ignore_user_abort();
+
+	
+	require_once './libs/class.phpmailer.php';
+	require_once './libs/function.php';
+	
+	
+	//QQ邮件发送	
+	function QQmail($address,$toname,$new){
+		sendQQmail($address,$toname,$new);
+	}
+	
+	//Gmail邮件发送
+	function gmail($address,$toname,$new){
+		sendGmail($address,$toname,$new);
+	}
+
+
+	function sendMail(){
+		$new = "hello world......";
+		QQmail('14287*****@qq.com', 'user', $new);																		//发送邮件
+	}
+
+	
+	for($i = 0;$i<=500;$i++){
+		/*循环发送数据*/
+		sendMail();
+
+		echo "hello";
+
+	}
+
+
+?>
+</body>
+</html>
